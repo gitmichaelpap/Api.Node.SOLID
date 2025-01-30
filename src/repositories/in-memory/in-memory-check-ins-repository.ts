9f7abc9c64f4bdc93e51497d47d83d7793f5d737
Michael Pacheco <michaelpacheco.br@gmail.com>
2025-01-30 02:18:26 -0300

    class 19

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -5,16 +5,32 @@ import dayjs from 'dayjs';
 
 export class InMemoryCheckInsRepository implements CheckInsRepository {
     public items: CheckIn[] = []
+
+    async save(checkIn: CheckIn) {
+        const checkInIndex = this.items.findIndex((item) => item.id === checkIn.id)
+        if (checkInIndex >= 0) {
+          this.items[checkInIndex] = checkIn
+        }
+        return checkIn
+    }
+
+    async findById(id: string) {
+        const checkIn = this.items.find((item) => item.id === id)
+        if (!checkIn) {
+          return null
+        }
+        return checkIn
+    }
     
     async countByUserId(userId: string) {
         return this.items.filter((checkIn) => checkIn.user_id === userId).length
-      }
+    }
 
     async findManyByUserId(userId: string, page: number) {
         return this.items
           .filter((checkIn) => checkIn.user_id === userId)
           .slice((page - 1) * 20, page * 20)
-      }
+    }
 
     async findByUserIdOnDate(userId: string, date: Date) {
         const startOfDay = dayjs(date).startOf('date')
--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -3,9 +3,15 @@ import { Prisma, User } from "@prisma/client";
 import { UsersRepository } from "@/repositories/users-repository";
 
 export class PrismaUsersRepository implements UsersRepository {
-    findById(email: string): Promise<User | null> {
-        throw new Error("Method not implemented.");
-    }
+    
+    async findById(id: string) {        
+        const user = await prisma.user.findUnique({
+            where: {
+            id,
+            },
+        })
+        return user
+    }    
 
     async create(data: Prisma.UserCreateInput) {
         const user = await prisma.user.create({ data })
