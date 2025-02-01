3a9cadb4c1cd395873a149790d21547a89871489
Michael Pacheco <michaelpacheco.br@gmail.com>
2025-02-01 14:39:54 -0300

    class 22

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,9 +1,12 @@
 import { FastifyInstance } from "fastify";
 import { register } from "@/http/controllers/register.controller";
 import { authenticate } from "./controllers/authenticate.controller";
+import { profile } from "./controllers/profile.controller";
+import { verifyJwt } from '@/http/middlewares/verify-jwt'
 
 export async function appRoutes(app: FastifyInstance): Promise<void> {
     app.post("/users", register);
-
     app.post('/sessions', authenticate)
+
+    app.get('/me', { onRequest: [verifyJwt] }, profile)
 }
